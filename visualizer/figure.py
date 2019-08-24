0579b4cb587ee334dfffbdbcff2ec162c6b1d378
Charles Davenport <cdavenport090@gmail.com>
2019-08-24 11:42:20 -0400

    output layer
    populated with random outputs for now

--- visualizer/figure.py
+++ visualizer/figure.py
@@ -10,6 +10,7 @@ from data_loaders.MNIST_data_loader import MNIST_data_loader
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import numpy as np
 
 from keras.models import load_model
 
@@ -24,18 +25,17 @@ class LeNet_MNIST_Figure(object):
         
         self.init_figure()
         
-    def init_figure(self):
-        
-        
-       
+    def init_figure(self):       
         
         n_layer = self.model.conv_layers + self.model.dense_layers
         
         self.fig = plt.figure(figsize=(5 * n_layer, 5))
 
-        w = 1/n_layer
+        w = 1/n_layer # unit width allotted for each layer
         self.im_width = 0.8*w
         self.arr_width = 0.2*w
+        
+        #print("{} {}".format(self.im_width, self.arr_width)) #debug
         
         self.font_dict = {'fontsize' : 16,
                      'color' : 'black',
@@ -70,7 +70,7 @@ class LeNet_MNIST_Figure(object):
             self.add_arrow(left)
             
         # DENSE LAYER ACTIVATIONS
-        for i in range(self.model.dense_layers):
+        for i in range(self.model.dense_layers-1):
             i += self.model.conv_layers
             left = (i+1)*(self.im_width + self.arr_width)
             new_ax = self.fig.add_axes([left,1.0, self.im_width,1.0])
@@ -78,10 +78,39 @@ class LeNet_MNIST_Figure(object):
             #i -= self.model.conv_layers
             new_ax.set_title('Dense Layer {}'.format(i+1), fontdict=self.font_dict)
             #new_ax.axis('off')
+            
             # ARROW
             left = (i+2) * self.im_width + (i+1) * self.arr_width
             self.add_arrow(left)
             
+        # OUTPUT LAYER
+        # debug
+        pred = np.random.rand(1,10)
+        left = (n_layer) *(self.im_width + self.arr_width)
+        new_ax = self.fig.add_axes([left,1.0, self.im_width,1.0])
+        new_ax.matshow(pred)
+        new_ax.set_title('Output Layer', fontdict=self.font_dict)
+        new_ax.axis('on')
+        new_ax.set_yticks([])
+        new_ax.set_ylabel('')
+        new_ax.set_xticks(range(10))
+        new_ax.xaxis.set_ticks_position('bottom')
+        new_ax.spines['bottom'].set_color('black')
+        new_ax.tick_params(axis='x', colors='black')
+        pred_label = np.argmax(pred[0])
+        #true_label = np.argmax(y_test[test_ind])
+        new_ax.text(0,4, 'Prediction: ', fontdict=self.font_dict_s)
+        confidence = pred[0, pred_label] * 100
+        fd_b = {'fontsize' : 48, 
+                'weight' : 'bold',
+                'color' : 'black'}
+        if confidence > 98.0: 
+            new_ax.text(4,4, '{0}'.format(pred_label), fontdict=fd_b)
+        else:
+            new_ax.text(4,4, '[?]', fontdict=self.font_dict)
+        new_ax.text(0,5, 'Confidence: {0:.6f}%'.format(confidence), fontdict=self.font_dict_s)
+        
+        # debug
         self.fig.savefig("test.png", dpi=150, bbox_inches='tight', facecolor='white')
         
     def add_arrow(self, left):
@@ -99,7 +128,7 @@ class LeNet_MNIST_Figure(object):
     
 if __name__=="__main__":
     model = LeNet_MNIST()
-    model.load_weights('..\experiments\LeNet_MNIST-20190823-175823\LeNet_MNIST-weights.03-0.1166.hdf5')
+    model.load('..\experiments\LeNet_MNIST-20190823-175823\LeNet_MNIST-weights.03-0.1166.hdf5')
     dl = MNIST_data_loader()
     newFig = LeNet_MNIST_Figure(model, dl)
 
