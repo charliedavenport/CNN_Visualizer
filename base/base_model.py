0c077167934c8ebff7a5b99088e43d9dfc9e2195
Charles Davenport <cdavenport090@gmail.com>
2019-08-24 11:41:57 -0400

    build model from config

--- base/base_model.py
+++ base/base_model.py
@@ -6,6 +6,10 @@ Created on Fri Aug 23 10:31:04 2019
 based on the template provided by https://github.com/Ahmkel/Keras-Project-Template
 """
 
+from json import loads
+
+from keras.models import model_from_json
+
 class BaseModel(object):
     def __init__(self):
         #self.config = config
@@ -21,13 +25,27 @@ class BaseModel(object):
         print("Model saved")
 
     # load latest checkpoint from the experiment path defined in the config file
-    def load(self, checkpoint_path):
+    def load_weights(self, checkpoint_path):
         if self.model is None:
             raise Exception("You have to build the model first.")
 
         print("Loading model checkpoint {} ...\n".format(checkpoint_path))
         self.model.load_weights(checkpoint_path)
         print("Model loaded")
+        
+    def build_from_config(self, config_path):
+        print("Building model from config {} ....\n".format(config_path))
+        with open(config_path, 'r') as f:
+            #json_str = loads(f.read())
+            self.model = model_from_json(f.read())
+            print('Model Summary:')
+            print(self.model.summary())
+        
+    def export_config(self, file_path):
+        json_str = self.model.to_json()
+        with open(file_path, 'w+') as file:
+            file.write(json_str)
+        
 
     def build_model(self):
         raise NotImplementedError
--- models/LeNet_MNIST.py
+++ models/LeNet_MNIST.py
@@ -12,6 +12,8 @@ from keras.layers import (Dense, Conv2D, MaxPooling2D,
                           Dropout, Flatten)
 from keras.models import Sequential, load_model
 import keras.utils
+import os
+from datetime import datetime
 
 class LeNet_MNIST(BaseModel):
     
@@ -30,11 +32,17 @@ class LeNet_MNIST(BaseModel):
     conv_layers = 3
     dense_layers = 2
     
-    def __init__(self):
+    def __init__(self, from_config=False, save_config=False, config_dir='configs', config_name=None):
         super().__init__()
-        self.build_model()
+        if from_config and config_name is not None:
+            config_path = os.path.join(config_dir, config_name)
+            self.build_from_config(config_path)    
+        else:
+            self.build_model(save_config, config_dir)
         
-    def build_model(self):
+    def build_model(self, save_config, config_dir):
+        
+        print('\nBuilding new model...')
         self.model = Sequential()
         ## C1 ##
         self.model.add(Conv2D(6, self.KERNEL_SIZE, strides=self.KERNEL_STRIDES,
@@ -66,7 +74,23 @@ class LeNet_MNIST(BaseModel):
         self.model.compile(loss=keras.losses.categorical_crossentropy,
                            optimizer=keras.optimizers.Adam(lr=self.LEARN_RATE),
                            metrics=['accuracy'])
+        print('Model Summary:')
         print(self.model.summary())
-        
-    def load_weights(self, file_path):
-        self.model = load_model(file_path)
+        if save_config:
+            now = datetime.utcnow().strftime('%Y%m%d-%H%M%S')
+            config_path = os.path.join(config_dir, '{0}-{1}.json'.format(self.name, now))
+            print('Saving configuration in {}'.format(config_path))
+            self.export_config(config_path)
+
+
+
+if __name__=="__main__":
+    # Test the LeNet_MNIST class:
+    # from config
+    config_dir = '..\configs'
+    config_name = 'LeNet_MNIST-20190824-150631.json'
+    LeNet_MNIST(from_config=True, save_config=False, 
+                config_dir=config_dir, config_name=config_name)
+    # from scratch
+    LeNet_MNIST(False, False, config_dir, None)
+    
